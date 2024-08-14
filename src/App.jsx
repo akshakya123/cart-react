37522dfb113f04749137db548f58dabbf30df676
akshakya123 <abhishekhkumarshaky@gmail.com>
2024-08-15 01:52:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Card from './component/Card'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <> 
+    <h1 className='bg-blue-600 text-black p-4 rounded-2xl mb-6'>tailwindcss tester</h1>
+    <Card/>
+    </>
+  )
+}
+
+export default App
